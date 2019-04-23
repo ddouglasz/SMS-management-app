c29b074b3d0383f42232a2b09c2ff3fe6610fa1f
Douglas Egiemeh <douglasegiemeh@gmail.com>
2019-04-23 12:05:28 +0100

    - implement get one contact controller - implement get all contacts controller

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -19,22 +19,79 @@ class ContactController {
         });
 
         return contact
-        .save()
-        .then((newContact) => {
-            return res.status(200).json({
-                success: true,
-                message: 'New contact created successfully',
-                contact: newContact
+            .save()
+            .then((newContact) => {
+                return res.status(200).json({
+                    success: true,
+                    message: 'New contact created successfully',
+                    contact: newContact
+                });
+            })
+            .catch((error) => {
+                res.status(500).json({
+                    success: false,
+                    message: 'server error',
+                    error: error.message,
+                });
             });
-        })
-        .catch((error) => {
-            res.status(500).json({
-                success: false,
-                message: 'server error',
-                error: error.message,
-            });
-        });
     }
+
+    /** 
+    * @description test controller.
+    * @param {object} req - the sent request.
+    * @param {object} res - the expected response.
+    * @returns {object} - the return function.
+    */
+    //get all contacts controller
+    // eslint-disable-next-line require-jsdoc
+    static getAllContacts(req, res) {
+        Contact.find()
+            .select('_id name phoneNumber')
+            .then((allContacts) => {
+                return res.status(200).json({
+                    success: true,
+                    message: 'all contacts',
+                    Contacts: allContacts
+                });
+            })
+            .catch((error) => {
+                res.status(500).json({
+                    success: false,
+                    message: 'Server error, please try again',
+                    error: error.message,
+                });
+            })
+    }
+
+    /** 
+    * @description test controller.
+    * @param {object} req - the sent request.
+    * @param {object} res - the expected response.
+    * @returns {object} - the return function.
+    */
+    //get all contacts controller
+    // eslint-disable-next-line require-jsdoc
+    static getSingleContact(req, res) {
+        const { contactId } = req.params;
+        Contact.findById(contactId)
+            .select('id name phoneNumber')
+            .then((contactDetail) => {
+                return res.status(200).json({
+                    success: true,
+                    message: 'contact found',
+                    Contact: contactDetail,
+                });
+            })
+            .catch((error) => {
+                res.status(500).json({
+                    success: false,
+                    message: 'Data does not exist',
+                    error: error.message,
+                });
+            })
+    }
+
+    //update controller
 }
 
 export default ContactController;
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,7 +4,9 @@ const  router = express.Router();
 import ContactController from '../controllers/contactController';
 
 router.post('/', ContactController.createContact);
-// router.get('/', ContactController.getAllContacts);
-// router.post('/:contactId', ContactController.deleteContact);
+router.get('/', ContactController.getAllContacts);
+router.get('/:contactId', ContactController.getSingleContact);
+// router.put('/:contactId', ContactController.updateContact);
+// router.delete('/:contactId', ContactController.deleteContact);
 
 export default router;
